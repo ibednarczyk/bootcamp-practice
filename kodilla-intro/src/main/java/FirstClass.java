30d8d6b05b1c0dac05302406699d1498a1f41464
Izabela Bednarczyk <izabela.bednarczyk93@gmail.com>
2019-11-16 18:44:39 +0100

    Rozbudowa kodu

---
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -0,0 +1,44 @@
+import com.sun.tools.javac.util.JCDiagnostic;
+
+public class FirstClass {
+    public static void main(String[] args) {
+            Notebook modernNotebook = new Notebook(1200, 700, 2019);
+            System.out.println("Parameters of the modern notebook: price: "+modernNotebook.price+"pln, weight: "+modernNotebook.weight+"g, manufacture year: "+modernNotebook.year+"yr");
+            modernNotebook.checkPrice();
+            modernNotebook.checkWeight();
+            modernNotebook.checkYear();
+
+
+            Notebook oldNotebook = new Notebook(500, 2000, 2000);
+            System.out.println("Parameters of the old notebook: "+oldNotebook.price+"pln, weight: "+oldNotebook.weight+"g, manufacture year: "+oldNotebook.year+"yr");
+            oldNotebook.checkPrice();
+            oldNotebook.checkWeight();
+            oldNotebook.checkYear();
+
+
+            Notebook averageNotebook = new Notebook(800,1600, 2010);
+            System.out.println("Parameters of the average notebook: price: "+averageNotebook.price+"pln, weight: "+averageNotebook.weight+"g, manufacture year: "+averageNotebook.year+"yr");
+            averageNotebook.checkPrice();
+            averageNotebook.checkWeight();
+            averageNotebook.checkYear();
+
+
+            Notebook smallNotebook = new Notebook(13.3, "portable");
+            System.out.println("Parameters of small notebook: screen size: "+smallNotebook.screenSize+" inches");
+            smallNotebook.checkScreenSize();
+            smallNotebook.checkPurpose();
+
+            Notebook mediumNotebook = new Notebook(15.6, "work");
+            System.out.println("Parameters of medium notebook: screen size: "+mediumNotebook.screenSize+" inches");
+            mediumNotebook.checkScreenSize();
+            mediumNotebook.checkPurpose();
+
+            Notebook bigNotebook = new Notebook(17.3, "gaming");
+            System.out.println("Parameters of big notebook: screen size: "+bigNotebook.screenSize+" inches");
+            bigNotebook.checkScreenSize();
+            bigNotebook.checkPurpose();
+
+
+    }
+}
+
---
+++ kodilla-intro/src/main/java/Notebook.java
@@ -0,0 +1,66 @@
+public class Notebook {
+    int price;
+    int weight;
+    int year;
+    double screenSize;
+    String purpose;
+
+    public Notebook(int price, int weight, int year) {
+        this.weight = weight;
+        this.price = price;
+        this.year = year;
+
+    }
+    public void checkPrice() {
+        if(this.price<600){
+            System.out.println("This notebook is very cheap.");
+        } else if(this.price>600 && this.price<1000) {
+            System.out.println("The price is good.");
+        } else {
+            System.out.println("This notebook is expensive.");
+        }
+    }
+    public void checkWeight() {
+        if (this.weight < 800) {
+            System.out.println("This notebook is light.");
+        } else if (this.weight> 800 && this.weight<1700) {
+            System.out.println("This notebook is quite light.");
+        } else {
+            System.out.println("This notebook is heavy.");
+        }
+    }
+    public void checkYear() {
+        if (this.year > 2018 && this.price > 1000){
+        System.out.println("It's a brand new notebook.");
+        } else if(this.year<2018 && this.year>=2010 && this.price>600 && this.price<1000) {
+            System.out.println("The price corresponds with a quality.");
+        } else {
+            System.out.println("This notebook is from older generation.");
+        }
+    }
+
+    public Notebook (double screenSize, String purpose){
+        this.screenSize = screenSize;
+        this.purpose = purpose;
+
+    }
+    public void checkScreenSize(){
+        if(this.screenSize<15.0) {
+            System.out.println("This notebook has a small screen, but it's really convenient.");
+        } else if(this.screenSize>15.0 && this.screenSize<17.0) {
+            System.out.println("This screen is bigger, so you can see wider picture.");
+        } else{
+            System.out.println("This notebook's screen is really big.");
+        }
+    }
+    public void checkPurpose(){
+        if(this.purpose == "portable" && this.screenSize<15.0){
+            System.out.println("This notebook is perfect if you work remotely.");
+        }
+        else if(this.purpose == "work" && this.screenSize>15.0 && this.screenSize<17.0){
+            System.out.println("This notebook if for people who work in the office.");
+        } else {
+            System.out.println("This notebook is the best choice for fans of games.");
+        }
+    }
+}
