84a99fd703bcebdba916b2f4dd26ec42c55de8ee
Izabela Bednarczyk <izabela.bednarczyk93@gmail.com>
2019-12-23 23:06:55 +0100

    homework_uservalidator_korekta

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/UserValidatorTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/UserValidatorTestSuite.java
@@ -3,6 +3,7 @@ package com.kodilla.parametrized_tests.homework;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
@@ -10,17 +11,26 @@ public class UserValidatorTestSuite {
     private UserValidator validator = new UserValidator();
 
     @ParameterizedTest
-    @ValueSource(strings = {"aT87.M"})
-    public void shouldValidateUserName(String username){
+    @ValueSource(strings = {"aT87.M", "website_12", "user..007", "resu-_-2"})
+    public void shouldValidateUserName(String username) {
         assertTrue(validator.validateUsername(username));
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"yivem.62788@seomail.net"})
-    public void shouldValidateEmail(String email){
+    @ValueSource(strings = {"yivem.62788@seomail.net", "4ben@sismeyapi.com", "hronald@newsletterbroadcaster.info", "ha.nalbta9n@alldc.websit"})
+    public void shouldValidateEmail(String email) {
         assertTrue(validator.validateEmail(email));
-
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"me", "1A", "%&*!", "ąęźx", " "})
+    public void shouldNotValidateUserNameWhenPassingWrongValue(String username) {
+        assertFalse(validator.validateUsername(username));
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"4bensismeyapicom", "8and@tjkjzjel.shooooop", "8and@tjkjzjel", "@tjkjzjel", " "})
+    public void shouldNotValidateEmailWhenPassingWrongValue(String email) {
+        assertFalse(validator.validateEmail(email));
+    }
 
 }
