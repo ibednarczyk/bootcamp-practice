2bdf7bc1164d7ae0e275221af53e7ff8879d7a49
Izabela Bednarczyk <izabela.bednarczyk93@gmail.com>
2020-01-11 19:26:44 +0100

    homework_calculator_bean3

--- kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_dependency_injection/homework/CalculatorTestSuite.java
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_dependency_injection/homework/CalculatorTestSuite.java
@@ -15,7 +15,7 @@ public class CalculatorTestSuite {
         ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
         Calculator bean = context.getBean(Calculator.class);
         double result = bean.add(14.5, 222.0);
-        assertEquals(236.5, result);
+        assertEquals(236.5, result, 0.01);
 
     }
 
@@ -24,7 +24,7 @@ public class CalculatorTestSuite {
        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
        Calculator bean = context.getBean(Calculator.class);
        double result = bean.subtract(-435.8, 64.2);
-       assertEquals(-500.0, result);
+       assertEquals(-500.0, result, 0.01);
 
    }
     @Test
@@ -32,7 +32,7 @@ public class CalculatorTestSuite {
         ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
         Calculator bean = context.getBean(Calculator.class);
         double result = bean.multiply(12.5, 10.0);
-        assertEquals(125.0, result);
+        assertEquals(125.0, result,0.01);
 
     }
     @Test
@@ -40,7 +40,7 @@ public class CalculatorTestSuite {
         ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
         Calculator bean = context.getBean(Calculator.class);
         double result = bean.divide(144.0, -12.0);
-        assertEquals(-12.0, result);
+        assertEquals(-12.0, result, 0.01);
 
     }
 }
