44df813660c9cc3751b18f8acf7754e2648f37a7
Izabela Bednarczyk <izabela.bednarczyk93@gmail.com>
2020-02-13 21:04:53 +0100

    13.4 XPath ćwiczenia_update

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -7,22 +7,29 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class AllegroTestingApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "/Users/i.bednarczyk/Dokumenty/Development/Kodilla/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("https://allegro.pl");
-        driver.findElement(By.xpath("//*[@data-box-name=\"allegro.rodoConsentModal\"]/div/div/div/div[2]/button[2]")).click();
+        while (!driver.findElement(By.xpath("//*[@data-box-name=\"allegro.rodoConsentModal\"]/div/div/div/button")).isDisplayed()) {
+
+        }
+        driver.findElement(By.xpath("//*[@data-box-name=\"allegro.rodoConsentModal\"]/div/div/div/button")).click();
 
         WebElement categoryDropdown = driver.findElement(By.xpath("//*[@class=\"_1h7wt _d25db_1HtwQ\"]/div[2]/div/select"));
         Select categorySelect = new Select(categoryDropdown);
         categorySelect.selectByIndex(3);
 
+        synchronized (driver)
+        {
+            driver.wait(2000);
+        }
+
         WebElement inputField = driver.findElement(By.xpath("//*[@class=\"_1h7wt _d25db_1HtwQ\"]/input"));
         inputField.sendKeys("mavic mini");
-
+        
         WebElement searchButton = driver.findElement(By.xpath("//*[@class=\"_1h7wt _d25db_1HtwQ\"]/button"));
         searchButton.submit();
-
 
     }
 }
